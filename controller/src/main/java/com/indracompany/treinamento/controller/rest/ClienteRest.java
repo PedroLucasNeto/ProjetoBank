d71e83e0322dd09856f4455b9c7a463710dcaada
PedroLucasNeto <pedrolukasneto@gmail.com>
2022-10-03 22:57:39 -0300

    update

--- controller/src/main/java/com/indracompany/treinamento/controller/rest/ClienteRest.java
+++ controller/src/main/java/com/indracompany/treinamento/controller/rest/ClienteRest.java
@@ -8,6 +8,8 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -38,5 +40,13 @@ public class ClienteRest extends GenericCrudRest<Cliente, Long, ClienteService>{
 		return new ResponseEntity<>(lista, HttpStatus.OK);
 	}
 	
+	/*@PostMapping(value = "/criaCliente/") 
+	public ResponseEntity<Cliente> criaCliente(@RequestBody Cliente novoCliente){
+		Cliente cliente = clienteService.criaClienteComConta(novoCliente);
+		if(clienteService.buscarClientePorCpf(novoCliente.getCpf())!=null) {
+			return new ResponseEntity<> (HttpStatus.FORBIDDEN);
+		}
+		return new ResponseEntity<>( HttpStatus.CREATED);
+	}*/
 
 }
